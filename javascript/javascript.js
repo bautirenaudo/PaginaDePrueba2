eb1ec11faca933da7eaa696855d982b6eae4f3e1
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2021-05-16 16:29:54 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,19 @@
 function main(){
-    //variables
+    //VARIABLES
     let nav=document.getElementById("nav");
     let iconMenu=document.getElementById("icon-menu");
     let iconMenu2=document.getElementById("icon-menu2");
 
-    //eventos
+    let circuloInstagram=document.getElementById("circulo-instagram");
+    let circuloFacebook=document.getElementById("circulo-facebook");
+    let circuloMail=document.getElementById("circulo-mail");
+    let circuloMenu=document.getElementById("circulo-menu");
+    let circuloMenu2=document.getElementById("circulo-menu2");
+
+
+    //EVENTOS
+
+    //menu pricipal
     iconMenu.onclick= function abrir(){
         nav.style.cssText="transform: translateY(0%);"
         iconMenu2.style.cssText=" visibility:visible ;";
@@ -19,5 +28,23 @@ function main(){
         
     }
 
+ 
+    
+    //menu redes sociales
+    circuloMenu.onclick= function abrir(){
+        circuloInstagram.style.cssText="transform: translateY(-200px);";
+        circuloFacebook.style.cssText="transform: translateY(-133px);";
+        circuloMail.style.cssText="transform: translateY(-66px);";
+        circuloMenu.style.cssText="visibility: hidden;";
+        circuloMenu2.style.cssText="visibility: visible;";
+    }
+
+    circuloMenu2.onclick= function abrir(){
+        circuloInstagram.style.cssText="transform: translateY(0%);";
+        circuloFacebook.style.cssText="transform: translateY(0%);";
+        circuloMail.style.cssText="transform: translateY(0%);";
+        circuloMenu.style.cssText="visibility: visible;";
+        circuloMenu2.style.cssText="visibility: hidden;";
+    }
 
 }
