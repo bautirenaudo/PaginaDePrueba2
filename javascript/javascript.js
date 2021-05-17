916d0ece145ca28de49486ddcca53f99d1e27988
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2021-05-17 20:20:28 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,19 +1,19 @@
 function main(){
     //variables
-    let header1=document.getElementById("header1");
+    let header2=document.getElementById("header2");
     let iconMenu=document.getElementById("icon-menu");
     let iconMenu2=document.getElementById("icon-menu2");
 
     //eventos
 
     iconMenu.onclick=function abrir(){
-        header1.style.cssText="transform: translateX(0%);"; 
+        header2.style.cssText="transform: translateX(0%);"; 
         iconMenu.style.cssText="visibility:hidden;";   
         iconMenu2.style.cssText="visibility:visible;"; 
     }
 
     iconMenu2.onclick=function cerrar(){
-        header1.style.cssText="transform: translateX(-100%);"; 
+        header2.style.cssText="transform: translateX(-100%);"; 
         iconMenu.style.cssText="visibility:visible;";   
         iconMenu2.style.cssText="visibility:hidden;"; 
     }
